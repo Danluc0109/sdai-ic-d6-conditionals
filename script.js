e08acc397f120ca4a18bcc5e79affd258db628e2
Danluc0109 <uyen.luc@gmail.com>
2024-11-07 05:03:09 +0000

    conditional

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // all code for this lab to be completed in this file
+const currentTemperature = 70;
+
+if (currentTemperature>32) {
+    console.log("It is not freezing...yet")
+} else {
+    console.log("It is freezing")
+}
+
+
+let myAge = 50;
+let yourAge = 36;
+
+if(myAge === yourAge) {
+    console.log("We are same age.")
+} else if (myAge > yourAge) {
+    console.log("I am older than you")
+} else {
+    console.log("I am younger than you")
+}
+
+let isLoggedIn = true
+
+if (isLoggedIn) {
+    console.log("Welcome back")
+} else {
+    console.log("Please Log in")
+}
